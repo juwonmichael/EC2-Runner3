632088542f20680f6f3401cde9b26bc9bcbfea5a
juwonmichael <juanmichael179@gmail.com>
2025-01-13 18:21:37 +0000

    iam role set

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "runnerrole" {
-  name               = "EC2RunnerRole" 
+resource "aws_iam_role" "runnerrole179" {
+  name               = "EC2RunnerRole179"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -34,8 +34,7 @@ resource "aws_security_group" "runner-sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    # Allow SSH access only from EC2 Instance Connect
-    cidr_blocks      = ["3.8.37.24/29"]  
+    cidr_blocks      = ["3.8.37.24/29"]
     ipv6_cidr_blocks = []
     self             = false
     prefix_list_ids  = []
@@ -47,8 +46,7 @@ resource "aws_security_group" "runner-sg" {
     from_port        = 9000
     to_port          = 9000
     protocol         = "tcp"
-    # Open SonarQube to the internet
-    cidr_blocks      = ["0.0.0.0/0"]  
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
     self             = false
     prefix_list_ids  = []
@@ -60,8 +58,7 @@ resource "aws_security_group" "runner-sg" {
     from_port        = 3000
     to_port          = 3000
     protocol         = "tcp"
-    # Open SonarQube to the internet
-    cidr_blocks      = ["0.0.0.0/0"]  
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
     self             = false
     prefix_list_ids  = []
@@ -80,18 +77,19 @@ resource "aws_security_group" "runner-sg" {
   }
 }
 
-
 resource "aws_instance" "runner-ec2" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = "t2.large"
-  # Replace below with your keypair name
   key_name               = "monday_keys"
   vpc_security_group_ids = [aws_security_group.runner-sg.id]
   iam_instance_profile   = aws_iam_instance_profile.runnerinstance-profile.name
+
   root_block_device {
     volume_size = 30
   }
+
   user_data = file("./InstallTools.sh")
+
   tags = {
     Name = "GITHUBRUNNER"
   }
